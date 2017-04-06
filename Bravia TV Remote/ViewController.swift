2fd253b9c7575c5c405e893ba2727c835712277e
Craig Rhodes <cdr2139@columbia.edu>
2017-04-06 12:34:57 -0400

    Initial Commit

---
+++ Bravia TV Remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bravia TV Remote
+//
+//  Created by Craig Rhodes on 4/6/17.
+//  Copyright © 2017 Craig Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
